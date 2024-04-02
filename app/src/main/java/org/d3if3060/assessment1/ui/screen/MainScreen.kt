010b30f441669a66e99e232282c72890028020a3
Darel Ajni Fahrezi <darelfahrezi@gmail.com>
2024-04-02 23:59:26 +0700

    Menambahkan input

--- app/src/main/java/org/d3if3060/assessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3060/assessment1/ui/screen/MainScreen.kt
@@ -1,18 +1,28 @@
 package org.d3if3060.assessment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if3060.assessment1.R
@@ -38,10 +48,38 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize().padding(16.dp)
-    ) {
 
+    var rupiah by remember {
+        mutableStateOf("")
+    }
+
+    var uangAsing by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.konverter_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = rupiah,
+            onValueChange = {rupiah = it},
+            label = { Text(text = stringResource(id = R.string.rupiah))},
+            leadingIcon = { Text(text = "Rp.")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
