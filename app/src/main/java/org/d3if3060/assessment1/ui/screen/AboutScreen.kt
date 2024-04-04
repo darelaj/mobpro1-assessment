f23cd0b0c10bfe08e5d8dd72f358bc14b51aaea7
Darel Ajni Fahrezi <darelfahrezi@gmail.com>
2024-04-04 19:10:46 +0700

    Menambah gambar pada tampilan about

--- app/src/main/java/org/d3if3060/assessment1/ui/screen/AboutScreen.kt
+++ app/src/main/java/org/d3if3060/assessment1/ui/screen/AboutScreen.kt
@@ -1,7 +1,12 @@
 package org.d3if3060.assessment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,7 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,12 +53,24 @@ fun AboutScreen(navController: NavHostController) {
             )
         }
     ) { padding ->
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(padding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Image(
+            painter = painterResource(id = R.drawable.konversi),
+            contentDescription = stringResource(id = R.string.gambar_konversi),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .size(160.dp)
+        )
         Text(
             text = stringResource(id = R.string.copyright),
-            modifier = Modifier
-                .padding(padding)
-                .padding(16.dp)
+            modifier = Modifier.padding(16.dp)
         )
+        }
     }
 }
 
